6afc49816fe57b5944431276ffa34b8e06bd6db0
Motoki Tokifuji <tokifuji@temona.co.jp>
2019-05-13 15:45:08 +0900

    added recovery command feature

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ watch some process in Linux server and notify Chatwork :)
 
 ### manually
 
-You can watch if destinated proccess is alive.  Below is an example of watching `sidekiq`:
+You can watch if destinated proccess is alive.
 
 ```
-$ ./path/to/proc_watch.sh sidekiq
+$ ./path/to/proc_watch.sh
 ```
 
 ### normally
@@ -17,7 +17,7 @@ $ ./path/to/proc_watch.sh sidekiq
 You might wanna add it to your `crontab`:
 
 ```
-*/1   *  *  *  *  cd /path/to/proc_watch_to_slack/ && ./proc_watch.sh sidekiq
+*/1   *  *  *  *  cd /path/to/proc_watch_to_slack/ && ./proc_watch.sh
 ```
 
 ## Installations
@@ -26,6 +26,8 @@ Just a few environment variables :
 
 1. `CHATWORK_ROOM_ID`: as you can read it, the ID of the Chatwork room you want it to post to.
 2. `CHATWORK_APP_TOKEN`: also you'll need to get the Chatwork App Token.
+3. `TARGET_NAME`: name of the target process ex: `sidekiq`
+4. `TARGET_RECOVERY_COMMAND`: recovery command when `TARGET_NAME` is dead.  Leave blank if you do not require to recover.
 
 Please configure above variables in the `.env` file.  You can `cp .env.sample .env && vim .env` to
 configure :)
--- proc_watch.sh
+++ proc_watch.sh
@@ -6,10 +6,10 @@ if [ ! -e ${SCRIPT_DIR}/.env ]; then
   exit 1
 fi
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 process_name" 1>&2
-  exit 1
-fi
+#if [ $# -ne 1 ]; then
+#  echo "Usage: $0 process_name" 1>&2
+#  exit 1
+#fi
 
 . ${SCRIPT_DIR}/.env
 
@@ -29,17 +29,19 @@ function chatworkSendMessage() {
     "https://api.chatwork.com/v2/rooms/${ROOM_ID}/messages"
 }
 
-PROCESS_NAME=$1
-CHATWORK_MESSAGE_HEADER="[info][title][${HOSTNAME}] ${PROCEES_NAME} watch :o[/title]"
+CHATWORK_MESSAGE_HEADER="[info][title][${HOSTNAME}] ${TARGET_NAME} watch :o[/title]"
 CHATWORK_MESSAGE_FOOTER="[/info]"
 
-COUNT=`ps -ef | grep ${PROCESS_NAME} | grep -v grep | grep -v $0 | wc -l`
-if [ ${COUNT} = 0 -a ! -f ${HOSTNAME}_${PROCESS_NAME}_err.log ]; then
-  chatworkSendMessage ${CHATWORK_APP_TOKEN} ${CHATWORK_ROOM_ID} "${CHATWORK_MESSAGE_HEADER}Process Not Found!! process_name: ${PROCESS_NAME}${CHATWORK_MESSAGE_FOOTER}"
-  echo `date '+%y/%m/%d %H:%M:%S'` 2> ${SCRIPT_DIR}/${HOSTNAME}_${PROCESS_NAME}_err.log
+COUNT=`ps -ef | grep ${TARGET_NAME} | grep -v grep | grep -v $0 | wc -l`
+if [ ${COUNT} = 0 -a ! -f ${HOSTNAME}_${TARGET_NAME}_err.log ]; then
+  chatworkSendMessage ${CHATWORK_APP_TOKEN} ${CHATWORK_ROOM_ID} "${CHATWORK_MESSAGE_HEADER}Process Not Found!! process_name: ${TARGET_NAME}${CHATWORK_MESSAGE_FOOTER}"
+  echo `date '+%y/%m/%d %H:%M:%S'` 2> ${SCRIPT_DIR}/${HOSTNAME}_${TARGET_NAME}_err.log
 else
-  if [ -e ${SCRIPT_DIR}/${HOSTNAME}_${PROCESS_NAME}_err.log ]; then
-    chatworkSendMessage ${CHATWORK_APP_TOKEN} ${CHATWORK_ROOM_ID} "${CHATWORK_MESSAGE_HEADER}Process recovered!! process_name: ${PROCESS_NAME}${CHATWORK_MESSAGE_FOOTER}"
-    rm ${SCRIPT_DIR}/${HOSTNAME}_${PROCESS_NAME}_err.log
+  if [ -e ${SCRIPT_DIR}/${HOSTNAME}_${TARGET_NAME}_err.log ]; then
+    rm ${SCRIPT_DIR}/${HOSTNAME}_${TARGET_NAME}_err.log
+  fi
+  if [[ -z "$TARGET_RECOVERY_COMMAND" ]]; then
+    $TARGET_RECOVERY_COMMAND
+    chatworkSendMessage ${CHATWORK_APP_TOKEN} ${CHATWORK_ROOM_ID} "${CHATWORK_MESSAGE_HEADER}Process recovered!! process_name: ${TARGET_NAME}${CHATWORK_MESSAGE_FOOTER}"
   fi
 fi
